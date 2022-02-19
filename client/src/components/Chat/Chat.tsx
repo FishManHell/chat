c146fe153558bc514f4cbdfb70708b4c31af6a06
FishManHell <leonard66693@gmail.com>
2022-02-20 00:43:51 +0200

    write code

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -6,7 +6,7 @@ import MessageList from "./MessageList";
 import useSocket from "../../hooks/useSocket";
 import {Token} from "../../typing/Interfaces";
 
-const Chat:FC<Token> = ({token, clearToken}) => {
+const Chat: FC<Token> = ({token, clearToken}) => {
     const [arrayMessage, sendMessageSocket, value, disconnectSocket, onChange] = useSocket('', token);
 
     function handleExitPage(): void {
@@ -19,10 +19,13 @@ const Chat:FC<Token> = ({token, clearToken}) => {
             <div className={'container'}>
                 <div className={'wrapper_chat_page__wrapper_block_chat'}>
                     <div className={'wrapper_chat_page__block_input_and_button_send'}>
-                        <InputChat value={value} change={onChange}/>
-                        <div className={'wrapper_chat_page__main_block_buttons'}>
-                            <SendButton message={sendMessageSocket} text={'Send'} value={value}/>
-                            <Logout exit={handleExitPage} text={'Logout'}/>
+                        <div className={'wrapper_chat_page__wrapper_input_buttons'}>
+                            <InputChat value={value} change={onChange}/>
+                            <div className={'wrapper_chat_page__main_block_buttons'}>
+                                <SendButton message={sendMessageSocket} text={'Send'} value={value}/>
+                                <Logout exit={handleExitPage} text={'Logout'}/>
+                            </div>
+
                         </div>
                         <MessageList array={arrayMessage}/>
                     </div>
